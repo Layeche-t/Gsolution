9738d52b73dcf0fd091ba264e7456412200a4ba9
Layeche Torki <layeche.torki@gmail.com>
2021-06-17 13:55:45 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>G7solution</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+</html>
+
+<footer>
+			<!-- Le premier bloc -->
+				<div class="bloc1">
+					<div class="certifications">
+						<h2 class="titre-bloc1"><a class="titre-bloc1-a">Certifications</a></h2>
+
+						<div class="logos">
+						<img class="qualiopi1" src="qualiopi.png">
+						<img class="qualiopi2" src="data.jpg">
+						</div>
+
+						<h2 class="nos-offres">Nos offres</h2>
+						<h2 class="sous-text"><a href="#">Formations à distence</a></h2>
+						<h2 class="sous-text"><a href="#">Nos services</a></h2>
+						<h2 class="sous-text"><a href="#">Sur mesure</a></h2>
+					</div>
+				</div>
+
+				<!-- Le deuxième bloc -->
+				<div class="bloc2">
+					<h2 class="g7solution">G7solution</h2>
+					<h2 class="sous-text"><a href="#">Nous sommes</a></h2>
+					<h2 class="sous-text"><a href="equipe.php">L'équipe</a></h2>
+					<h2 class="sous-text"><a href="#">Nos experts</a></h2>
+					<h2 class="sous-text"><a href="blog.php">Blog</a></h2>
+				</div>
+
+
+				<!-- Le troisème bloc -->
+				<div class="bloc3">
+					<h2 class="titre-bloc3"><a class="titre-bloc1-a">Rejoignez-nous</a></h2>
+
+					<div class="logos">
+						<i class="fab fa-facebook-square ico"></i>
+						<i class="fab fa-twitter-square ico"></i>
+						<i class="fab fa-linkedin ico"></i>
+						<i class="fab fa-youtube ico"></i>
+					</div>
+
+					<h2 class="nos-offres">En savoir plus</h2>
+					<h2 class="sous-text"><a href="faq.php">FAQ</a></h2>
+					<h2 class="sous-text"><a href="formulaire_contact.php">Contactez-nous</a></h2>
+					<h2 class="sous-text"><a href="#">Le plan du site</a></h2>
+				</div>
+	</footer>
+</body>
+			
