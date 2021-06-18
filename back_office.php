3daeba4598e3ad8152b960605f338ad06866eb4d
Layeche Torki <layeche.torki@gmail.com>
2021-06-18 15:10:11 +0200

    layeche

--- back_office.php
+++ back_office.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<title>Back office</title>
 	<link rel="stylesheet" type="text/css" href="style_office.css">
+	<script src="https://kit.fontawesome.com/5da465d417.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -17,10 +18,19 @@
 		</header>
 
 		<nav>
-			
+			<li class="nav"> <a href="#"> <i class="fas fa-home"></i> Accueil</a></li>
+			<li class="nav"> <a href="#"> <i class="fas fa-tablet-alt"></i> Slide</a></li>
+			<li class="nav"> <a href="#"> <i class="fas fa-store"></i> Formation</a></li>
+			<li class="nav"> <a href="#"> <i class="fas fa-user-friends"></i> L'équipe</a></li>
+			<li class="nav"> <a href="#"> <i class="fab fa-blogger-b"></i> Blog</a></li>
+			<li class="nav"> <a href="#"> <i class="far fa-calendar-alt"></i> Planning</a></li>
+			<li class="nav"> <a href="#"> <i class="fas fa-user-edit"></i> Espace client</a></li>
+			<li class="nav"> <a href="#"> <i class="far fa-user"></i> Espace stagiaire</a></li>
+			<li class="nav"> <a href="#"> <i class="fas fa-user-alt-slash"></i> Gérer les comptes</a></li>
 		</nav>
 
 		<section>
+			
 			
 		</section>
 
