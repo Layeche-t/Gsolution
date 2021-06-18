c175ee43a41818a9a56f2e90764afd85468594da
Layeche Torki <layeche.torki@gmail.com>
2021-06-18 14:16:37 +0200

    new fils

---
+++ back_office.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Back office</title>
+	<link rel="stylesheet" type="text/css" href="style_office.css">
+</head>
+<body>
+
+	<div class="container">
+		<header>
+			<h1 class="titre-nom">Bonjour Gilles !</h1>
+			<li class="menu1"><a href="#">Tableau de bord</a> </li>
+			<li class="menu1"><a href="#">Planning</a></li>
+			<li class="menu1"><a href="#">Messages</a></li>
+			<li class="deconnexion"><a href="#">Déconnexion</a> </li>
+		</header>
+
+		<nav>
+			
+		</nav>
+
+		<section>
+			
+		</section>
+
+		<footer>
+			
+		</footer>
+	</div>
+
+</body>
+</html>
