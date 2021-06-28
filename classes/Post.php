74c9b9ac82297a20d03b201675f73b7c3e6c2cf8
Layeche Torki <layeche.torki@gmail.com>
2021-06-28 10:55:25 +0200

    add class Post

---
+++ classes/Post.php
@@ -0,0 +1,90 @@
+<?php
+
+// table
+class post
+{
+
+    private $id;
+
+    private $titel;
+
+    private $source;
+
+    private $discription;
+
+    private $picture;
+
+    private $createdAt;
+
+    private $foring;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getTitel()
+    {
+        return $this->titel;
+    }
+
+    public function setTitel(string $titel)
+    {
+        $this->titel = $titel;
+    }
+
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    public function setSource(string $source)
+    {
+        $this->source = $source;
+    }
+
+    public function getDiscription()
+    {
+        return $this->discription;
+    }
+
+    public function setDiscription(string $discription)
+    {
+        $this->discription = $discription;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        return $this->createdAt = $createdAt;
+    }
+
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    public function setPicture($picture)
+    {
+        $this->id = $picture;
+    }
+
+    public function getForing()
+    {
+        return $this->foring;
+    }
+
+    public function setForinge(string $foring)
+    {
+        $this->foring = $foring;
+    }
+}
