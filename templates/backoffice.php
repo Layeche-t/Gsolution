40d52d62a48c220ca2cfe77adde0b85b94e41745
Layeche Torki <layeche.torki@gmail.com>
2021-06-28 13:47:09 +0200

    add file backoffice

---
+++ templates/backoffice.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Back office</title>
+    <link rel="stylesheet" type="text/css" href="../styles/style_office.css">
+    <script src="https://kit.fontawesome.com/5da465d417.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <div class="container">
+        <header>
+            <h1 class="titre-nom">Bonjour Gilles !</h1>
+            <li class="menu1"><a class="a-header" href="#">Tableau de bord</a> </li>
+            <li class="menu1"><a class="a-header" href="#">Planning</a></li>
+            <li class="menu1"><a class="a-header" href="#">Messages</a></li>
+            <li class="deconnexion"><a class="deco" href="#">Déconnexion</a> </li>
+        </header>
+
+        <nav>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-home"></i> Accueil</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-tablet-alt"></i> Slide</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-store"></i> Formation</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-user-friends"></i> L'équipe</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="fab fa-blogger-b"></i> Blog</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="far fa-calendar-alt"></i> Planning</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-user-edit"></i> Espace client</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="far fa-user"></i> Espace stagiaire</a></li>
+            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-user-alt-slash"></i> Gérer les comptes</a></li>
+        </nav>
+
+        <section>
+            <h1 class="titre-section">Formation</h1>
+
+            <label class="label-titre">Entrez votre sujet</label>
+            <input class="choix-titre" type="" name="">
+
+            <label class="label-titre">Choisissez votre photo</label>
+            <input class="choix-titre" type="file" name="">
+
+            <textarea></textarea>
+
+            <button value="">Valider</button>
+        </section>
+
+        <footer>
+
+        </footer>
+    </div>
+
+</body>
+
+</html>
