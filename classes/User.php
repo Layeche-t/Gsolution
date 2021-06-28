78264c8f1cb08641d686acf45c71d2eed739dbd8
Layeche Torki <layeche.torki@gmail.com>
2021-06-28 13:33:10 +0200

    modification of file

--- classes/User.php
+++ classes/User.php
@@ -3,8 +3,9 @@
 /**
  * class user 
  */
-class User
+class User extends AbstractDataBase
 {
+    const TABLE = 'users';
 
     private $id;
 
@@ -18,6 +19,15 @@ class User
 
     private $email;
 
+    private $password;
+
+    private $createdAt;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function getId()
     {
         return $this->id;
@@ -26,5 +36,75 @@ class User
     public function setId(int $id)
     {
         $this->id = $id;
+    }
+
+    public function getFirtName()
+    {
+        return $this->firtName;
+    }
+
+    public function setFirtName($firtName)
+    {
+        $this->firtName = $firtName;
+    }
+
+    public function getlastName()
+    {
+        return $this->lastname;
+    }
+
+    public function setlastName($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe($sexe)
+    {
+        $this->sexe = $sexe;
+    }
+
+    public function getVisitor()
+    {
+        return $this->visitor;
+    }
+
+    public function setVisitor($visitor)
+    {
+        $this->visitor = $visitor;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(int $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getcreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setcreatedAt(int $createdAt)
+    {
+        $this->createdAt = $createdAt;
     }
 }
