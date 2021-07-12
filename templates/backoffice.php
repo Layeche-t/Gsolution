ceb6ac49885f2eaebc3d0afd57955118e4ec65e6
Layeche Torki <layeche.torki@gmail.com>
2021-07-12 16:20:25 +0200

    creation of the dashboard

--- templates/backoffice.php
+++ templates/backoffice.php
@@ -1,60 +1,43 @@
-
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <title>Back office</title>
-    <link rel="stylesheet" type="text/css" href="../styles/style_office.css">
-    <script src="https://kit.fontawesome.com/5da465d417.js" crossorigin="anonymous"></script>
-</head>
-
 <body>
 
-    <div class="container">
-        <header>
-            <h1 class="titre-nom">Bonjour Gilles !</h1>
-            <li class="menu1"><a class="a-header" href="#">Tableau de bord</a> </li>
-            <li class="menu1"><a class="a-header" href="#">Planning</a></li>
-            <li class="menu1"><a class="a-header" href="#">Messages</a></li>
-            <li class="deconnexion"><a class="deco" href="#">Déconnexion</a> </li>
-        </header>
-
-        <nav>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-home"></i> Accueil</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-tablet-alt"></i> Slide</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-store"></i> Formation</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-user-friends"></i> L'équipe</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="fab fa-blogger-b"></i> Blog</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="far fa-calendar-alt"></i> Planning</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-user-edit"></i> Espace client</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="far fa-user"></i> Espace stagiaire</a></li>
-            <li class="nav"> <a class="a-nav" href="#"> <i class="fas fa-user-alt-slash"></i> Gérer les comptes</a></li>
-        </nav>
-
-        <section>
-            <form method="POST" action="">
-                <h1 class="titre-section">Formation</h1>
-
-
-                <label class="label-titre">Entrez votre sujet</label>
-                <input class="choix-titre" type="text" name="titel">
-
-                <label class="label-titre">Choisissez votre photo</label>
-                <input class="choix-titre" type="file" name="">
-
-                <textarea name="text"></textarea>
-
-                <button value="" name="validation">Valider</button>
-            </form>
-        </section>
-
-        <footer>
-
-        </footer>
+    <?php include 'header_backOffice.php'; ?>
+    <div class="container-fluid div-container">
+        <div class='dashboard'>
+            <div class="dashboard-nav">
+                <header><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a><a href="#" class="brand-logo"><i class="fas fa-anchor"></i> <span>BRAND</span></a></header>
+                <nav class="dashboard-nav-list"><a href="#" class="dashboard-nav-item"><i class="fas fa-home"></i>
+                        Home </a><a href="#" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i> dashboard
+                    </a><a href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Upload </a>
+                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-photo-video"></i> Media </a>
+                        <div class='dashboard-nav-dropdown-menu'><a href="#" class="dashboard-nav-dropdown-item">All</a><a href="#" class="dashboard-nav-dropdown-item">Recent</a><a href="#" class="dashboard-nav-dropdown-item">Images</a><a href="#" class="dashboard-nav-dropdown-item">Video</a></div>
+                    </div>
+                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-users"></i> Users </a>
+                        <div class='dashboard-nav-dropdown-menu'><a href="#" class="dashboard-nav-dropdown-item">All</a><a href="#" class="dashboard-nav-dropdown-item">Subscribed</a><a href="#" class="dashboard-nav-dropdown-item">Non-subscribed</a><a href="#" class="dashboard-nav-dropdown-item">Banned</a><a href="#" class="dashboard-nav-dropdown-item">New</a></div>
+                    </div>
+                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-money-check-alt"></i> Payments </a>
+                        <div class='dashboard-nav-dropdown-menu'><a href="#" class="dashboard-nav-dropdown-item">All</a><a href="#" class="dashboard-nav-dropdown-item">Recent</a><a href="#" class="dashboard-nav-dropdown-item"> Projections</a>
+                        </div>
+                    </div>
+                    <a href="#" class="dashboard-nav-item"><i class="fas fa-cogs"></i> Settings </a><a href="#" class="dashboard-nav-item"><i class="fas fa-user"></i> Profile </a>
+                    <div class="nav-item-divider"></div>
+                    <a href="#" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i> Logout </a>
+                </nav>
+            </div>
+            <div class='dashboard-app'>
+                <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a></header>
+                <div class='dashboard-content'>
+                    <div class='container'>
+                        <div class='card'>
+                            <div class='card-header'>
+                                <h1>Welcome back Jim</h1>
+                            </div>
+                            <div class='card-body'>
+                                <p>Your account type is: Administrator</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
 </body>
-
-</html>
---
+++ templates/header_backOffice.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/5da465d417.js" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../styles/styleOffice.css">
+    <title>Header</title>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
