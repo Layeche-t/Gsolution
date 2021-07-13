14c85e18fcb0315d89f78064788d930c78eb21aa
Layeche Torki <layeche.torki@gmail.com>
2021-07-13 09:41:08 +0200

    Improvement of the back office

--- templates/backoffice.php
+++ templates/backoffice.php
@@ -1,30 +1,49 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/5da465d417.js" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/styleOffice.css">
+    <title>BackOffice</title>
+</head>
+
+
 <body>
 
-    <?php include 'header_backOffice.php'; ?>
+
     <div class="container-fluid div-container">
         <div class='dashboard'>
             <div class="dashboard-nav">
-                <header><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a><a href="#" class="brand-logo"><i class="fas fa-anchor"></i> <span>Gilles</span></a></header>
-                <nav class="dashboard-nav-list"><a href="#" class="dashboard-nav-item"><i class="fas fa-home"></i>
-                        Accueil </a><a href="#" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i> Slide
-                    </a><a href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Formation </a>
-                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-photo-video"></i> Media </a>
-                        <div class='dashboard-nav-dropdown-menu'><a href="#" class="dashboard-nav-dropdown-item">All</a><a href="#" class="dashboard-nav-dropdown-item">Recent</a><a href="#" class="dashboard-nav-dropdown-item">Images</a><a href="#" class="dashboard-nav-dropdown-item">Video</a></div>
-                    </div>
-                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-users"></i> Utilisataires </a>
-                        <div class='dashboard-nav-dropdown-menu'><a href="#" class="dashboard-nav-dropdown-item">All</a><a href="#" class="dashboard-nav-dropdown-item">Subscribed</a><a href="#" class="dashboard-nav-dropdown-item">Non-subscribed</a><a href="#" class="dashboard-nav-dropdown-item">Banned</a><a href="#" class="dashboard-nav-dropdown-item">New</a></div>
-                    </div>
-                    <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-money-check-alt"></i> Payments </a>
-                        <div class='dashboard-nav-dropdown-menu'><a href="#" class="dashboard-nav-dropdown-item">All</a><a href="#" class="dashboard-nav-dropdown-item">Recent</a><a href="#" class="dashboard-nav-dropdown-item"> Projections</a>
-                        </div>
-                    </div>
-                    <a href="#" class="dashboard-nav-item"><i class="fas fa-cogs"></i> Settings </a><a href="#" class="dashboard-nav-item"><i class="fas fa-user"></i> Profile </a>
+                <header>
+                    <a href="#" class="brand-logo"><i class="fas fa-anchor"></i><span>Gilles</span></a>
+                </header>
+                <!--Menu-->
+                <nav class="dashboard-nav-list">
+
+                    <a href="#" class="dashboard-nav-item"><i class="fas fa-home"></i>Accueil </a>
+                    <a href="#" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i> Slide</a>
+                    <a href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Services </a>
+                    <a href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Formations</a>
+                    <a href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Equipe</a>
+                    <a href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Utilisateurs</a>
+
+                    <!-- Vertical bar -->
                     <div class="nav-item-divider"></div>
                     <a href="#" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i> Déconnexion </a>
                 </nav>
+
+                <!-- The heart of the dashboard -->
             </div>
             <div class='dashboard-app'>
-                <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a></header>
+                <header class='dashboard-toolbar'>
+                    <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
+                </header>
+
                 <div class='dashboard-content'>
                     <div class='container'>
                         <div class='card'>
@@ -41,3 +60,5 @@
         </div>
     </div>
 </body>
+
+</html>
