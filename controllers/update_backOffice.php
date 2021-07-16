91683260091e9464a1721942047d8d9f1824c913
Layeche Torki <layeche.torki@gmail.com>
2021-07-16 19:20:30 +0200

    upload picture

---
+++ controllers/update_backOffice.php
@@ -0,0 +1,15 @@
+<?php
+require_once('../inc_config.php');
+
+$post = new Post();
+
+if (isset($_POST['modification'])) {
+
+    unset($_POST['modification']);
+
+    $update = $post->updateById( $_POST, $post::TABLE);
+    header('Location: ../admin/slide_disply.php?success');
+    exit();
+
+}
+?>
