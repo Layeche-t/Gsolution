664c2c3a282b5eddeb09ef6886a994c2c3a3da11
Layeche Torki <layeche.torki@gmail.com>
2021-07-19 11:00:48 +0200

    cleaning the dashbord

--- admin/slide_disply.php
+++ admin/slide_disply.php
@@ -1,27 +1,28 @@
-
 <?php
+
+//connxion à la base de données
 require_once('../inc_config.php');
+//l'objet
 $post = new Post();
 
-$affichages= $post->findAll($post::TABLE);
+//méthode select par
+$sliders = $post->findBy(['type' => 'slider'], 1000, $post::TABLE);
 
-
+//supprimer un élement du tableau
 if (isset($_GET['id'])) {
     $post->deleteById($_GET['id'], $post::TABLE);
-    header('Location: ../admin/slide_disply.php');
+    header('Location: ../admin/slide_disply.php?delete');
+    exit();
 }
-
 ?>
 
 <!-- import du header -->
 <?php include('inc_header.php'); ?>
 
-
-
 <!-- Le contenu du dashbord-->
 <div class='dashboard-app'>
     <header class='dashboard-toolbar'>
-        <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
+        <a href="#" class="menu-toggle"><i class="fas fa-bars"></i></a>
     </header>
 
     <div class='dashboard-content'>
@@ -31,114 +32,112 @@ if (isset($_GET['id'])) {
                 <!--titre du dashbord -->
                 <div class='card-header'>
                     <h1>Le contenu de votre slide</h1>
-                    <?php if (isset($_GET['success'])) :?>
-                    <div class="alert alert-success" role="alert">
-                        A simple success alert—check it out!
-                    </div>
+
+                    <!--success ajout-->
+                    <?php if (isset($_GET['success'])) : ?>
+                        <div class="alert alert-success" role="alert">
+                            Votre slide vient d'être ajouté
+                        </div>
                     <?php endif ?>
 
+                    <!--success suppression-->
+                    <?php if (isset($_GET['delete'])) : ?>
+                        <div class="alert alert-success" role="alert">
+                            Votre slide vient d'être supprimé
+                        </div>
+                    <?php endif ?>
 
+                    <div class='card-body'>
+                        <!-- bouton d'envoie (pop up) -->
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <button type="button" class="btn btn-primary submit-ajout" data-bs-toggle="modal" data-bs-target="#exampleModal">Ajouter </button>
+                        </div>
 
+                        <!--contenu du tableau -->
+                        <table class="table">
+                            <!-- le header du tableau -->
+                            <thead class="table-dark">
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Titre</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Source de l'image</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
 
-                <div class='card-body'>
+                            <tbody>
+                                <!-- la boucle foreach pour l'affichage -->
+                                <?php
+                                $num = 0;
+                                foreach ($sliders as $slider) :
+                                    $num = $num + 1;
+                                ?>
 
-                    <!-- bouton d'envoie (pop up) -->
-                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                        <button type="button" class="btn btn-primary submit-ajout" data-bs-toggle="modal" data-bs-target="#exampleModal" >Ajouter </button>
+                                    <tr>
+                                        <th scope="row"> <?= $num ?> </th>
+                                        <td> <?= $slider['titel'] ?> </td>
+                                        <td> <?= $slider['description'] ?> </td>
+                                        <td> <?= $slider['source'] ?> </td>
+                                        <td>
+                                            <a href="slide_modification.php?id= <?= $slider['id'] ?>"><button type="button" class="btn btn-success">Modifier</button></a>
+                                            <a href="?id= <?= $slider['id'] ?>"><button type="button" class="btn btn-danger">Supprimer</button></a>
+                                        </td>
+                                    </tr>
+
+                                <?php endforeach ?>
+                            </tbody>
+                        </table>
+                        <!-- fin du tableau -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- pop up d'ajout -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!--titre du formulaire -->
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Nouveau contenu</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
 
-                    <!--contenu du tableau -->
+                    <!-- formulaire d'envoie-->
+                    <div class="modal-body">
+                        <form action="../controllers/add_backOffice.php" method="POST" enctype="multipart/form-data">
 
+                            <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">Entrez votre titre :</label>
+                                <input type="text" class="form-control" id="recipient-name" name="titel" required>
+                            </div>
 
-
-
-
-                    <table class="table">
-
-                        <thead class="table-dark">
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Titre</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Source de l'image</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-
-                        <?php foreach( $affichages as $affichage) : ?>
-
-                        <tr>
-                            <th scope="row"> <?= $affichage['id'] ?> </th>
-                            <td> <?= $affichage['titel'] ?> </td>
-                            <td> <?= $affichage['description'] ?> </td>
-                            <td> <?= $affichage['source'] ?> </td>
-                            <td>
-                                <a href="slide_modification.php?id= <?=$affichage['id']?>"><button type="button" class="btn btn-success">Modifier</button></a>
-                                <a href="?id= <?=  $affichage['id'] ?>"><button type="button" class="btn btn-danger">Supprimer</button></a>
-                            </td>
-                        </tr>
-
-                       <?php endforeach ?>
-                        </tbody>
-                    </table>
-                    <!-- fin du tableau -->
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <!--titre du formulaire -->
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Nouveau contenu</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-
-                <!-- formulaire d'envoie-->
-                <div class="modal-body">
-                    <form action="../controllers/add_backOffice.php" method="POST">
-
-                        <div class="mb-3">
-                            <label for="recipient-name" class="col-form-label">Entrez votre titre :</label>
-                            <input type="text" class="form-control" id="recipient-name" name="titel" required>
-                        </div>
-
-                        <div class="input-group mb-3">
+                            <div class="input-group mb-3">
                                 <input type="file" class="form-control input-file" id="inputGroupFile02" accept="image/*" name="picture">
-                        </div>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="message-text" class="col-form-label">La description : </label>
-                            <textarea class="form-control" id="message-text" name="description" required ></textarea>
-                        </div>
+                            <div class="mb-3">
+                                <label for="message-text" class="col-form-label">La description : </label>
+                                <textarea class="form-control" id="message-text" name="description" required></textarea>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="message-text" class="col-form-label">Votre contenu : </label>
-                            <textarea class="form-control" id="message-text" rows="5" cols="33" name="text" required></textarea>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary" name="validation">Valider</button>
-                        </div>
-
-                    </form>
+                            <div class="mb-3">
+                                <label for="message-text" class="col-form-label">Votre contenu : </label>
+                                <textarea class="form-control" id="message-text" rows="5" cols="33" name="text" required></textarea>
+                            </div>
+                            <input name="type" value="slider" hidden>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-primary" name="validation">Valider</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-
-                <!-- bouton d'envoie -->
-
-
+                <!-- end -->
             </div>
-            <!-- end -->
         </div>
+
+        <!--import du footer-->
+        <?php include('inc_footer.php'); ?>
     </div>
-
-    <!--import du footer-->
-    <?php include ('inc_footer.php'); ?>
-
-
-</div>
