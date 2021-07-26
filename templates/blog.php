1cc5109cc55f49de71edf6acd018d1b9b2239732
Layeche Torki <layeche.torki@gmail.com>
2021-07-26 13:23:14 +0200

    migration of the file to the bootstrap and data recovery

--- templates/blog.php
+++ templates/blog.php
@@ -1,65 +1,71 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>G7solution</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="https://kit.fontawesome.com/5da465d417.js" crossorigin="anonymous"></script>
-</head>
+<!--Header + menu-->
+<?php
+include_once '../inc_config.php';
 
-<body>
+$post = new Post();
 
-	<!--Header + menu-->
-		<?php include 'header.php';?>
+$blogs = $post->findBy(['type' => 'blog'], 1000, $post::TABLE);
+?>
 
-		<!-- formulaire d'inscription -->
+<?php include 'inc_header.php'; ?>
 
-			<h1 class="titre-blog"> <span class="span-blog">Blog</span></h1>
-			<div class="blog">
-				<div class="presentation">
-					<h1 class="title"><span class="span-blog">Le développement durable</span></h1>
-					<img class="image-presention" src="logo-1.png">
-					<p>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue <br><br><a href="pageblog.php">Lire plus...</a> </p>
+
+<section class="mt-4" id="gallery">
+	<div class="container">
+		<div class="row">
+
+			<h1 class="card-title mb-3"> Blog </h1>
+
+			<?php foreach ($blogs as $blog) : ?>
+
+				<div class="col-lg-4 mb-4">
+					<div class="card">
+						<img src="https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+						<div class="card-body">
+							<h5 class="card-title"> <?= $blog['titel'] ?> </h5>
+							<p class="card-text"><?= $blog['description'] ?></p>
+							<a href="" class="btn btn-outline-success btn-sm">Read More</a>
+							<a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+						</div>
+					</div>
 				</div>
-				<div class="presentation">
-					<h1 class="title"><span class="span-blog">Le développement durable</span></h1>
-					<img class="image-presention" src="logo-1.png">
-					<p>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue <br><br><a href="#">Lire plus...</a> </p>
-				</div>
-				<div class="presentation">
-					<h1 class="title"><span class="span-blog">Le développement durable</span></h1>
-					<img class="image-presention" src="logo-1.png">
-					<p>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue <br><br><a href="#">Lire plus...</a> </p>
-				</div>
-				<div class="presentation">
-					<h1 class="title"><span class="span-blog">Le développement durable</span></h1>
-					<img class="image-presention" src="logo-1.png">
-					<p>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue <br><br><a href="#">Lire plus...</a></p>
+
+			<?php endforeach ?>
+
+			<!-- <div class="col-lg-4 mb-4">
+				<div class="card">
+					<img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Sunset</h5>
+						<p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eum similique repellat a laborum, rerum voluptates ipsam eos quo tempore iusto dolore modi dolorum in pariatur. Incidunt repellendus praesentium quae!</p>
+						<a href="" class="btn btn-outline-success btn-sm">Read More</a>
+						<a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+					</div>
 				</div>
 			</div>
-
-		<div class="contenaire-pagination">
-			  <ul class="pagination">
-			    <li class="icon">
-			      <a href="#"><span class="fas fa-angle-left"></span> Précédent</a>
-			    </li>
-			    <li><a href="#">1</a></li>
-			    <li><a href="#">2</a></li>
-			    <li><a href="#">3</a></li>
-			    <li><a href="#">4</a></li>
-			    <li><a href="#">5</a></li>
-			    <li><a href="#">6</a></li>
-			    <li><a href="#">7</a></li>
-			    <li><a href="#">8</a></li>
-			    <li><a href="#">9</a></li>
-			    <li><a href="#">10</a></li>
-			    <li class="icon">
-			      <a href="#">Suivant <span class="fas fa-angle-right"></span></a>
-			    </li>
-			  </ul>
+			<div class="col-lg-4 mb-4">
+				<div class="card">
+					<img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title">Sunset</h5>
+						<p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eum similique repellat a laborum, rerum voluptates ipsam eos quo tempore iusto dolore modi dolorum in pariatur. Incidunt repellendus praesentium quae!</p>
+						<a href="" class="btn btn-outline-success btn-sm">Read More</a>
+						<a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+					</div>
+				</div>
+			</div> -->
 		</div>
+	</div>
+</section>
 
-	<?php include 'footer.php';?>
+
+
+
+
+</div>
+
+
 
 </body>
+
 </html>
