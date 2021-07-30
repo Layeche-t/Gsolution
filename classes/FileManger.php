e18df3982032ed541209e6492fc2e7302554f866
Layeche Torki <layeche.torki@gmail.com>
2021-07-30 14:24:53 +0200

    new class for file manger(picture)

--- classes/FileManger.php
+++ classes/FileManger.php
@@ -1,7 +1,33 @@
 <?php
 
-
+/**
+ * Class FileManger
+ */
 class FileManger
 {
+    /**
+     * @param $file
+     */
+    public function UploadImage(array $file): string
+    {
+        $tabExtension = explode('.', $file['name']);
+        $extension = strtolower(end($tabExtension));
+        $extensionAutrosiee = ['png', 'jpg', 'jpeg'];
+        $tailleMx = 50000;
+        $uniqueName = uniqid('', true);
+        $fileName = $uniqueName . '.' . $extension;
+        $target_dir = '../upload/' . $fileName;
+
+
+        if (in_array($extension, $extensionAutrosiee) && $file['size'] < $tailleMx && $file['error'] == 0) {
+
+            move_uploaded_file($file['tmp_name'], $target_dir);
+
+        }
+
+        return $fileName;
+    }
+
+
 
 }
