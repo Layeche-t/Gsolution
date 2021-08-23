ad370bec90bccb8e44495b613046a01461edb1a0
Layeche Torki <layeche.torki@gmail.com>
2021-08-23 11:28:41 +0200

    update of header

---
+++ templates/inc_header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>G7solution</title>
+    <link rel="stylesheet" href="../styles/styleBootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</head>
+
+
+<body>
+
+    <div class="container-fluid container-general ">
+        <div class="container-fluid bg-primary">
+
+            <div class="row">
+                <div class="col">
+                    <a href="#">
+                        <img src="../pictures/logo.png" alt="" width="300" height="100">
+                    </a>
+                </div>
+                <div class="col" style="background-color:orange;">
+                    <a href="#">
+                        <p class=" font-weight-bolder text-right text-client ">Espace client</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark justify-content-center" style="border-radius:0px;">
+
+            <!-- Links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link 2</a>
+                </li>
+
+                <!-- Dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        Dropdown link
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+
+
+</body>
+
+</html>
+
+</div>
+
+
+</body>
