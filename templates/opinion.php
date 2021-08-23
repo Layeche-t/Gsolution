88f755e3a8fbe83c3c05edd35c821beaabfbdb75
Layeche Torki <layeche.torki@gmail.com>
2021-08-23 21:17:18 +0200

    creat Testimonial Carousel

---
+++ templates/opinion.php
@@ -0,0 +1,63 @@
+<div class="container mt-5 border">
+    <h3 class="mb-4 font-weight-bold titel"> Ce qu'ils disent de nous </h3>
+    <div class="col-lg-10 offset-lg-1 bg-dark text-light">
+        <div id="client-testimonial-carousel" class="carousel slide" data-ride="carousel" style="height:200px;">
+            <div class="carousel-inner" role="listbox">
+
+                <div class="carousel-item active text-center p-5">
+                    <blockquote class="blockquote text-center">
+                        <p class="mb-0"><i class="fa fa-quote-left"></i> Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.
+                        </p>
+                        <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+                        <!-- Client review stars -->
+                        <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+                        <p class="client-review-stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                            <i class="far fa-star"></i>
+                        </p>
+                    </blockquote>
+                </div>
+                <div class="carousel-item text-center p-4">
+                    <blockquote class="blockquote text-center">
+                        <p class="mb-0"><i class="fa fa-quote-left"></i> Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.
+                        </p>
+                        <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+                        <!-- Client review stars -->
+                        <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+                        <p class="client-review-stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </p>
+                    </blockquote>
+                </div>
+                <div class="carousel-item text-center p-4">
+                    <blockquote class="blockquote text-center">
+                        <p class="mb-0"><i class="fa fa-quote-left"></i> A person who never made a mistake never tried anything new.
+                        </p>
+                        <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+                        <!-- Client review stars -->
+                        <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
+                        <p class="client-review-stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </p>
+                    </blockquote>
+                </div>
+            </div>
+            <ol class="carousel-indicators">
+                <li data-target="#client-testimonial-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#client-testimonial-carousel" data-slide-to="1"></li>
+                <li data-target="#client-testimonial-carousel" data-slide-to="2"></li>
+            </ol>
+        </div>
+    </div>
+</div>
