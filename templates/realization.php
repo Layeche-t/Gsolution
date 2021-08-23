3093b399b939537ed8c9009f21ae8f5785ae0ae7
Layeche Torki <layeche.torki@gmail.com>
2021-08-23 16:24:05 +0200

    creation of the realization part

---
+++ templates/realization.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <h3 class="mb-3 font-weight-bold titel">Réalisations </h3>
+    <div class="row">
+        <div class="col m-3">
+            <div class="counter-box"> <i class="fa fa-user"> </i> <span class="counter">2147</span>
+                <p>Personnes formées</p>
+            </div>
+        </div>
+        <div class="col m-3">
+            <div class="counter-box"> <i class="fa fa-group"></i> <span class="counter">3275</span>
+                <p>Clients</p>
+            </div>
+        </div>
+        <div class="col m-3">
+            <div class="counter-box"> <i class="fa fa-shopping-cart"></i> <span class="counter">289</span>
+                <p>Partenaires</p>
+            </div>
+        </div>
+        <div class="col m-3">
+            <div class="counter-box"> <i class="fa fa-thumbs-o-up"></i> <span class="counter">1563</span>
+                <p>Satisfactions</p>
+            </div>
+        </div>
+    </div>
+</div>
