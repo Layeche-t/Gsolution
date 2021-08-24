95d0dcdc2991391f35dc0a8c6dc673e92f70bdc6
Layeche Torki <layeche.torki@gmail.com>
2021-08-24 12:00:21 +0200

    migration of the footer to bootstrap

---
+++ templates/inc_footer.php
@@ -0,0 +1,161 @@
+<footer class="container-fluid bg-grey py-5">
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-md-6">
+				<div class="row">
+					<div class="col-md-6 ">
+						<div class="logo-part">
+							<img src="../pictures/qualiopi.png" class="w-50 logo-footer">
+							<p>7637 Laurel Dr. King Of Prussia, PA 19406</p>
+							<p>Use this tool as test data for an automated system or find your next pen</p>
+						</div>
+					</div>
+					<div class="col-md-6 px-4">
+						<h6> Nos offres</h6>
+						<p>But horizontal lines can only be a full pixel high.</p>
+					</div>
+					<div class="col-md-6 ">
+						<div class="social">
+							<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+							<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+						</div>
+					</div>
+
+
+
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="row">
+					<div class="col-md-6 px-4">
+						<h6> Help us</h6>
+						<div class="row ">
+							<div class="col-md-6">
+								<ul>
+									<li> <a href="#"> Home</a> </li>
+									<li> <a href="#"> About</a> </li>
+									<li> <a href="#"> Service</a> </li>
+									<li> <a href="#"> Team</a> </li>
+									<li> <a href="#"> Help</a> </li>
+									<li> <a href="#"> Contact</a> </li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+
+
+					<div class="col-md-6 ">
+						<h6> Newsletter</h6>
+
+						<div class="row ">
+							<div class="col-md-6">
+								<ul>
+									<li> <a href="#"> Home</a> </li>
+									<li> <a href="#"> About</a> </li>
+									<li> <a href="#"> Service</a> </li>
+									<li> <a href="#"> Team</a> </li>
+									<li> <a href="#"> Help</a> </li>
+									<li> <a href="#"> Contact</a> </li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<span>Copyright © 2019, All Right Reserved Seobin</span>
+				</div>
+				<!-- End Col -->
+				<div class="col-md-6">
+					<div class="copyright-menu">
+						<ul>
+							<li>
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#">Terms</a>
+							</li>
+							<li>
+								<a href="#">Privacy Policy</a>
+							</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- End col -->
+			</div>
+			<!-- End Row -->
+		</div>
+		<!-- End Copyright Container -->
+	</div>
+	<!-- End Copyright -->
+	<!-- Back to top -->
+	<div id="back-to-top" class="back-to-top">
+		<button class="btn btn-dark" title="Back to Top" style="display: block;">
+			<i class="fa fa-angle-up"></i>
+		</button>
+	</div>
+	</div>
+</footer>
+
+
+
+
+
+<!-- Le premier bloc -->
+<!-- <div class="bloc1">
+		<div class="certifications">
+			<h2 class="titre-bloc1"><a class="titre-bloc1-a">Certifications</a></h2>
+
+			<div class="logos">
+				<img class="qualiopi1" src="../pictures/qualiopi.png">
+				<img class="qualiopi2" src="../pictures/data.jpg">
+			</div>
+
+			<h2 class="nos-offres">Nos offres</h2>
+			<h2 class="sous-text"><a href="#">Formations à distence</a></h2>
+			<h2 class="sous-text"><a href="#">Nos services</a></h2>
+			<h2 class="sous-text"><a href="#">Sur mesure</a></h2>
+		</div>
+	</div> -->
+
+<!-- Le deuxième bloc -->
+<!-- <div class="bloc2">
+		<h2 class="g7solution">G7solution</h2>
+		<h2 class="sous-text"><a href="#">Nous sommes</a></h2>
+		<h2 class="sous-text"><a href="../templates/team.php">L'équipe</a></h2>
+		<h2 class="sous-text"><a href="#">Nos experts</a></h2>
+		<h2 class="sous-text"><a href="blog.php">Blog</a></h2>
+	</div> -->
+
+
+<!-- Le troisème bloc -->
+<!-- <div class="bloc3">
+		<h2 class="titre-bloc3"><a class="titre-bloc1-a">Rejoignez-nous</a></h2>
+
+		<div class="logos">
+			<i class="fab fa-facebook-square ico"></i>
+			<i class="fab fa-twitter-square ico"></i>
+			<i class="fab fa-linkedin ico"></i>
+			<i class="fab fa-youtube ico"></i>
+		</div>
+
+		<h2 class="nos-offres">En savoir plus</h2>
+		<h2 class="sous-text"><a href="faq.php">FAQ</a></h2>
+		<h2 class="sous-text"><a href="formulaire_contact.php">Contactez-nous</a></h2>
+		<h2 class="sous-text"><a href="#">Le plan du site</a></h2>
+	</div>
+</footer>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
