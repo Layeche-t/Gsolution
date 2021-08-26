ce87a659d6d13fe72b6c1f1bce028ce26188d786
Layeche Torki <layeche.torki@gmail.com>
2021-08-26 22:18:07 +0200

    added datetime function

--- controllers/add.php
+++ controllers/add.php
@@ -2,19 +2,29 @@
 require_once('../inc_config.php');
 
 $user = new User();
+$now = new DateTime();
 
 if (isset($_POST['validation'])) {
 
     unset($_POST['validation']);
 
-    if ($_POST['password'] != $_POST['confirmPassword']){
+
+    if ($_POST['password'] != $_POST['confirmPassword']) {
         header('Location: ../templates/registrationForm.php?error=pw');
         exit();
     }
+
     unset($_POST['confirmPassword']);
-    $newPost = $user->SetUser($_POST);
+    $check = $user->findOneBy(['email' => $_POST['email']], $user::TABLE);
+
+    if ($check) {
+        header('Location: ../templates/registrationForm.php?error=em');
+        exit();
+    }
+
+    $_POST['createdAt'] = $now->format('Y-m-d H:i:s');
+    $newUser = $user->SetUser($_POST);
 
     header('Location: ../templates/form_autho.php?success');
     exit;
-
 }
