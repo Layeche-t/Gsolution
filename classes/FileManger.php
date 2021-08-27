ec12de4637beeccce1d458622ec27b09413663f3
Layeche Torki <layeche.torki@gmail.com>
2021-08-27 21:56:18 +0200

    addadding conditions for errors

--- classes/FileManger.php
+++ classes/FileManger.php
@@ -10,24 +10,31 @@ class FileManger
      */
     public function UploadImage(array $file): string
     {
+
         $tabExtension = explode('.', $file['name']);
+
         $extension = strtolower(end($tabExtension));
+
         $extensionAutrosiee = ['png', 'jpg', 'jpeg'];
-        $tailleMx = 50000;
+        $tailleMx = 500000; //a voir avec macy
         $uniqueName = uniqid('', true);
         $fileName = $uniqueName . '.' . $extension;
         $target_dir = '../upload/' . $fileName;
 
 
-        if (in_array($extension, $extensionAutrosiee) && $file['size'] < $tailleMx && $file['error'] == 0) {
+        if (in_array($extension, $extensionAutrosiee)) {
 
-            move_uploaded_file($file['tmp_name'], $target_dir);
 
+            if ($file['size'] < $tailleMx && $file['error'] == 0) {
+                move_uploaded_file($file['tmp_name'], $target_dir);
+            } else {
+                header('Location: ../admin/slide_disply.php?error=siz');
+                exit();
+            }
+        } else {
+            header('Location: ../admin/slide_disply.php?error=ext');
+            exit();
         }
-
         return $fileName;
     }
-
-
-
 }
