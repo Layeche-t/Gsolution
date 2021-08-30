1b8cba7cec9223bb2fa78f11ad374f4cfbb3e00e
Layeche Torki <layeche.torki@gmail.com>
2021-08-30 17:08:17 +0200

    removing the footer and adjusting the div

--- templates/forgot_password.php
+++ templates/forgot_password.php
@@ -1,23 +1,29 @@
 <?php include 'inc_header.php'; ?>
 
 <div class="container padding-bottom-3x mb-5 mt-5">
-    <?php if (isset($_GET['success'])) : ?>
-        <div class="alert alert-success" role="alert">
-            Un nouveau mot de passe vient d'être envoyé à votre adresse email !
-        </div>
-    <?php endif; ?>
 
-    <?php if (isset($_GET['error']) && $_GET['error'] == 'no') : ?>
-        <div class="alert alert-danger text-center font-weight-bold" role="alert">
-            Votre mot de passe n'est pas
-        </div>
-    <?php endif ?>
 
     <div class="row justify-content-center ">
         <div class="col-lg-7 col-md-10 px-0 color-forgot1 border">
             <h2 class="text-center font-weight-bold color-forgot card-header"> Mot de passe oublié </h2>
-            <div class="container mt-3">
+            <div class="container  mt-3">
+                <?php if (isset($_GET['success'])) : ?>
+                    <div class="alert alert-success text-center font-weight-bold" role="alert">
+                        Un lien vient d'être envoyé à votre adresse email !
+                    </div>
+                <?php endif; ?>
 
+                <?php if (isset($_GET['error']) && $_GET['error'] == 'vid') : ?>
+                    <div class="alert alert-danger text-center font-weight-bold" role="alert">
+                        Veulliez saisir votre mail !
+                    </div>
+                <?php endif ?>
+
+                <?php if (isset($_GET['error']) && $_GET['error'] == 'noc') : ?>
+                    <div class="alert alert-danger text-center font-weight-bold" role="alert">
+                        Cet mail n'exsiste pas!
+                    </div>
+                <?php endif ?>
                 <p class="">Changez votre mot de passe en trois étapes faciles : </p>
                 <ol class="list-unstyled font-weight-bold">
                     <li><span class="text-primary text-medium"> </span> 1- Entrez votre adresse mail associé à votre compte ci-dessous.</li>
@@ -30,9 +36,9 @@
                 <div class="card-body">
                     <div class="form-group"> <label for="email-for-pass">Entrez votre adresse email : </label> <input class="form-control" type="text" name="email" required=""><small class="form-text text-muted"></small> </div>
                 </div>
-                <div class="card-footer text-center color-forgot">
-                    <button class="btn btn-success" type="submit" name="envoyer">POURSUIVRE</button>
-                    <button class="btn btn-danger" type="submit">ABANDONNER</button>
+                <div class=" text-center mb-4">
+                    <button class="btn btn-success mr-2" type="submit" name="envoyer">POURSUIVRE</button>
+                    <button class="btn btn-danger ml-2" type="submit">ABANDONNER</button>
                 </div>
             </form>
         </div>
