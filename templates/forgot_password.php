0fac885ee17d94ad9dd614badef897cf98553790
Layeche Torki <layeche.torki@gmail.com>
2021-08-30 12:02:38 +0200

    bootstrap setting

--- templates/forgot_password.php
+++ templates/forgot_password.php
@@ -1,6 +1,6 @@
 <?php include 'inc_header.php'; ?>
 
-<div class="container padding-bottom-3x mb-2 mt-5">
+<div class="container padding-bottom-3x mb-5 mt-5">
     <?php if (isset($_GET['success'])) : ?>
         <div class="alert alert-success" role="alert">
             Un nouveau mot de passe vient d'être envoyé à votre adresse email !
@@ -13,22 +13,27 @@
         </div>
     <?php endif ?>
 
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
-            <div class="forgot">
-                <h2>Forgot your password?</h2>
-                <p>Change your password in three easy steps. This will help you to secure your password!</p>
-                <ol class="list-unstyled">
-                    <li><span class="text-primary text-medium">1. </span>Enter your email address below.</li>
-                    <li><span class="text-primary text-medium">2. </span>Our system will send you a temporary link</li>
-                    <li><span class="text-primary text-medium">3. </span>Use the link to reset your password</li>
+    <div class="row justify-content-center ">
+        <div class="col-lg-7 col-md-10 px-0 color-forgot1 border">
+            <h2 class="text-center font-weight-bold color-forgot card-header"> Mot de passe oublié </h2>
+            <div class="container mt-3">
+
+                <p class="">Changez votre mot de passe en trois étapes faciles : </p>
+                <ol class="list-unstyled font-weight-bold">
+                    <li><span class="text-primary text-medium"> </span> 1- Entrez votre adresse mail associé à votre compte ci-dessous.</li>
+                    <li><span class="text-primary text-medium"></span>2- Notre système vous enverra un lien temporaire.</li>
+                    <li><span class="text-primary text-medium"></span>3- Utilisez le lien pour réinitialiser votre mot de passe.</li>
                 </ol>
             </div>
-            <form action="../controllers/forgot.php" class="card mt-4" method="POST">
+
+            <form action="../controllers/forgot.php" class=" mt-4" method="POST">
                 <div class="card-body">
-                    <div class="form-group"> <label for="email-for-pass">Enter your email address</label> <input class="form-control" type="text" name="email" required=""><small class="form-text text-muted">Enter the email address you used during the registration on BBBootstrap.com. Then we'll email a link to this address.</small> </div>
+                    <div class="form-group"> <label for="email-for-pass">Entrez votre adresse email : </label> <input class="form-control" type="text" name="email" required=""><small class="form-text text-muted"></small> </div>
                 </div>
-                <div class="card-footer"> <button class="btn btn-success" type="submit" name="envoyer">Get New Password</button> <button class="btn btn-danger" type="submit">Back to Login</button> </div>
+                <div class="card-footer text-center color-forgot">
+                    <button class="btn btn-success" type="submit" name="envoyer">POURSUIVRE</button>
+                    <button class="btn btn-danger" type="submit">ABANDONNER</button>
+                </div>
             </form>
         </div>
     </div>
