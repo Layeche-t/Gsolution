e9e2e06c78e3a10f5747eaabc9f2538ae22c43c6
Layeche Torki <layeche.torki@gmail.com>
2021-08-31 21:50:50 +0200

    file to manage email management

--- controllers/recover.php
+++ controllers/recover.php
@@ -2,15 +2,17 @@
 require_once('../inc_config.php');
 
 $user = new User();
-$now = new DateTime();
+
 
 if (empty($_GET['token'])) {
-    header('Location:../templates/passwordChange.php?error=vidtok');
+    header('Location:../templates/forgot_password.php?error=notok');
     exit;
 }
 
 $check = $user->findOneBy(['token' => $_GET['token']], $user::TABLE);
 if (empty($check)) {
-    header('Location:../templates/passwordChange.php?error=notok');
+    header('Location:../templates/forgot_password.php?error=notok');
     exit;
 }
+header('Location:../templates/newPassword.php?token=' . $_GET['token']);
+exit;
