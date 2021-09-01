1eac65e0b4b584d7ab3cb59708286c60ad90dabf
Layeche Torki <layeche.torki@gmail.com>
2021-09-01 13:48:38 +0200

    ajout du bootstrap pour login

--- templates/inc_header.php
+++ templates/inc_header.php
@@ -9,9 +9,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://kit.fontawesome.com/5da465d417.js" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 
@@ -19,73 +19,78 @@
 
     <div class="container-fluid container-general">
         <div class="container-fluid div-logo">
-            <div class="row">
-                <div class="col">
+            <div class="row ">
+                <div class="col border">
                     <a href="#">
                         <img src="../pictures/logo.png" alt="" width="300" height="100">
                     </a>
                 </div>
-                <div class="col">
-                    <a href="#">
-                        <p class=" font-weight-bolder text-right text-client ">Espace client</p>
-                    </a>
+                <div class="col border ">
+
+                    <a href="#" data-toggle="dropdown" class="nav-item nav-link dropdown-toggle user-action" id="navbardrop"><i class="fas fa-user"></i> Antonio Moreno</a>
+                    <div class="dropdown-menu">
+                        <a href="#" class="dropdown-item"><i class="fa fa-user-o"></i> Profile</a>
+                        <a href="#" class="dropdown-item"><i class="fa fa-calendar-o"></i> Calendar</a>
+                        <a href="#" class="dropdown-item"><i class="fa fa-sliders"></i> Settings</a>
+                        <div class="divider dropdown-divider"></div>
+                        <a href="#" class="dropdown-item"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <nav class="navbar navbar-expand-lg justify-content-center menu" style="border-radius:0px;">
+            <nav class="navbar navbar-expand-lg justify-content-center menu" style="border-radius:0px;">
 
-            <!-- Links -->
-            <ul class="navbar-nav">
-                <li class="nav-item mx-2">
-                    <a class="nav-link nav-color font-weight-bold" href="#">ACCUEIL</a>
-                </li>
+                <!-- Links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link nav-color font-weight-bold" href="#">ACCUEIL</a>
+                    </li>
 
-                <!-- Dropdown -->
-                <li class="nav-item dropdown mx-2">
-                    <a class="nav-link dropdown-toggle nav-color font-weight-bold" href="#" id="navbardrop" data-toggle="dropdown">
-                        ORGANISME DE FORMATION
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
+                    <!-- Dropdown -->
+                    <li class="nav-item dropdown mx-2">
+                        <a class="nav-link dropdown-toggle nav-color font-weight-bold" href="#" id="navbardrop" data-toggle="dropdown">
+                            ORGANISME DE FORMATION
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Link 1</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+                    </li>
 
-                <li class="nav-item dropdown mx-2">
-                    <a class="nav-link dropdown-toggle nav-color font-weight-bold" href="#" id="navbardrop" data-toggle="dropdown">
-                        RH EXTERNISE
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
+                    <li class="nav-item dropdown mx-2">
+                        <a class="nav-link dropdown-toggle nav-color font-weight-bold" href="#" id="navbardrop" data-toggle="dropdown">
+                            RH EXTERNISE
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Link 1</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+                    </li>
 
-                <li class="nav-item dropdown mx-2">
-                    <a class="nav-link dropdown-toggle nav-color font-weight-bold" href="#" id="navbardrop" data-toggle="dropdown">
-                        CONSEILS
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
+                    <li class="nav-item dropdown mx-2">
+                        <a class="nav-link dropdown-toggle nav-color font-weight-bold" href="#" id="navbardrop" data-toggle="dropdown">
+                            CONSEILS
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Link 1</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+                    </li>
 
-                <li class="nav-item mx-2">
-                    <a class="nav-link nav-color font-weight-bold" href="#">PLANNING</a>
-                </li>
-                <li class="nav-item mx-2">
-                    <a class="nav-link nav-color font-weight-bold" href="#">A PROPOS</a>
-                </li>
-                <li class="nav-item mx-2">
-                    <a class="nav-link nav-color font-weight-bold" href="#">CONTACT</a>
-                </li>
-            </ul>
-        </nav>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link nav-color font-weight-bold" href="#">PLANNING</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link nav-color font-weight-bold" href="#">A PROPOS</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link nav-color font-weight-bold" href="#">CONTACT</a>
+                    </li>
+                </ul>
+            </nav>
 
 </body>
 
