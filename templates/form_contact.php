e70ff5fcc363abcf5c906efdfa4c9a6bb7597a65
Layeche Torki <layeche.torki@gmail.com>
2021-09-01 09:06:25 +0200

    add two closing tags body and html

--- templates/form_contact.php
+++ templates/form_contact.php
@@ -1,54 +1,76 @@
 <!--Header + menu-->
-<?php include 'header.php'; ?>
+<?php include 'inc_header.php'; ?>
 
 <!-- formulaire d'inscription -->
 
 <h1 class="titre-blog"> <span class="span-blog">Contactez-nous</span></h1>
 
-<div class="contenaire-contact">
+<div class="card card0">
+	<div class="d-flex flex-lg-row flex-column-reverse">
+		<div class="card card1">
+			<div class="row justify-content-center my-auto">
+				<div class="col-md-9 col-10 my-5">
+					<h2 class="mb-2 text-center font-weight-bold heading">Bienvenu !</h2>
+					<h3 class="mb-5 text-center heading">Vous n'êtes pas encore inscrit</h3>
 
-	<form class="form-contact">
-		<h1 class="titre-form-inscription">Contactez-nous </h1>
+					<form method="POST" action="../controllers/add.php">
 
-		<div class="div-inscription">
-			<label>Choisissez votre sujet :</label>
-			<select class="liste-deroulante">
-				<option>Recrutement</option>
-				<option>Formations</option>
-				<option>Conseils</option>
-				<option>Autres</option>
-			</select>
+						<div class="my-3">
+							<label class="form-check-label py-1 mr-2 ml-2 "> Votre sexe </label>
+							<div class="form-check-inline py-1 px-2">
+								<input type="radio" class="form-check-input" name="sexe" value="0" checked> <label class="form-check-label">M
+								</label>
+							</div>
+							<div class="form-check-inline">
+								<input type="radio" class="form-check-input" name="sexe" value="1"> <label class="form-check-label">F
+								</label>
+							</div>
+						</div>
+
+						<div class="my-3">
+							<label class="form-check-label py-1 mr-2 ml-2 "> Vous êtes ? </label>
+							<div class="form-check-inline py-1 ">
+								<input type="radio" class="form-check-input" name="role" value="stagaire" checked> <label class="form-check-label">Stagiaire
+								</label>
+							</div>
+							<div class="form-check-inline">
+								<input type="radio" class="form-check-input" name="role" value="client"> <label class="form-check-label">Client
+								</label>
+							</div>
+						</div>
+
+
+						<div class="form-group"> <label class="form-control-label  ">Nom</label> <input type="text" id="email" name="firstname" placeholder="Nom" class="form-control" required> </div>
+						<div class="form-group"> <label class="form-control-label ">Prénom</label> <input type="text" id="psw" name="lastname" placeholder="Prénom" class="form-control" required> </div>
+						<div class="form-group"> <label class="form-control-label ">E-mail</label> <input type="email" id="email" name="email" placeholder="E-mail" class="form-control" required> </div>
+						<div class="form-group"> <label class="form-control-label ">Mot de passe</label> <input type="password" name="password" placeholder="Mot de passe" class="form-control" required> </div>
+						<div class="form-group"> <label class="form-control-label ">Confirmation de mot de passe</label> <input type="password" name="confirmPassword" placeholder="Mot de passe" class="form-control" required> </div>
+
+
+
+
+						<div class="row justify-content-center my-3 px-3"> <button class="btn-block btn-color" type="submit" name="validation">Enregistrer</button> </div>
+						<div class="row justify-content-center my-1"> <a class="a-color" href="#">Lire les conditions générales</a> </div>
+					</form>
+				</div>
+			</div>
 		</div>
 
-		<div class="div-inscription">
-			<label>Nom :</label><input class="input-inscription" type="password" name="" placeholder="Prénom">
+
+		<div class="card card2">
+			<div class="my-auto mx-md-5 px-md-2 right" style="margin: 0px;">
+				<h3 class="text-body font-weight-bold">We are more than just a company</h3> <small class="text-body font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</small>
+			</div>
+
+			<div class=" text-center mb-5">
+				<p href="#" class="sm-text mx-auto mb-1 font-weight-bold">Vous avez déja un compte ?<a href="form_autho.php"><button class="btn btn-white ml-2">Se connecter </button></a></p>
+			</div>
 		</div>
-
-		<div class="div-inscription">
-			<label>Prénom :</label><input class="input-inscription" type="password" name="" placeholder="E-mail">
-		</div>
-
-		<div class="div-inscription">
-			<label>E-mail :</label><input class="input-inscription" type="password" name="" placeholder="Mot de passe">
-		</div>
-
-		<div class="div-inscription">
-			<label>Message :</label> <textarea class="message" placeholder="message"></textarea>
-		</div>
-
-
-		<button class="retour-connexion">ENVOYER</button>
-	</form>
-
-	<div class="infos-contact">
-		<h1 class="titre-visiteur">Informations</h1>
-		<p class="p-connexion">Adress : 32 rue Riquet / 53 rue de la Colombette 31000 Toulouse <br><br> Adress e-mail : contact@g7solution.fr <br><br> Numéro de téléphone : 06 19 44 53 34 </p>
 	</div>
-
-
+</div>
 </div>
 <!-- Pied de page -->
-<?php include 'footer.php'; ?>
+<?php include 'inc_footer.php'; ?>
 
 
 
