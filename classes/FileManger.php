bf0662c3e6358a7f2b8d9941e688a5353ec8c599
Layeche Torki <layeche.torki@gmail.com>
2021-09-07 23:24:54 +0200

    new method for file management

--- classes/FileManger.php
+++ classes/FileManger.php
@@ -37,4 +37,42 @@ class FileManger
         }
         return $fileName;
     }
+
+    /**
+     * @param $file
+     */
+    public function UploadFile(array $file): string
+    {
+
+        $tabExtension = explode('.', $file['name']);
+
+        $extension = strtolower(end($tabExtension));
+
+
+        $extensionAutrosiee = ['pdf'];
+
+        $tailleMx = 1000000; //a voir avec macy
+
+        $uniqueName = uniqid('', true);
+
+        $fileName = $uniqueName . '.' . $extension;
+
+        $target_dir = '../upload/' . $fileName;
+
+
+
+
+        if (in_array($extension, $extensionAutrosiee)) {
+
+            if ($file['size'] < $tailleMx && $file['error'] == 0) {
+            } else {
+                header('Location: ../admin/_disply.php?error=siz');
+                exit();
+            }
+        } else {
+            header('Location: ../admin/training_disply.php?error=ext');
+            exit();
+        }
+        return $fileName;
+    }
 }
