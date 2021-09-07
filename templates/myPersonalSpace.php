35af7d184edf39394c7a4183055e185837d6495a
Layeche Torki <layeche.torki@gmail.com>
2021-09-07 09:40:15 +0200

    affichage des données de la formation

--- templates/myPersonalSpace.php
+++ templates/myPersonalSpace.php
@@ -1,10 +1,22 @@
 <?php
 require_once('../inc_config.php');
 
-if ($_SESSION['autoriser'] != 'oui') {
-    header('Location: ../templates/form_autho.php');
-    exit();
+$user = new User();
+$post = new Post();
+
+if (isset($_SESSION['user']['id'])) {
+    $curentuser = $user->findOneBy(['id' => $_SESSION['user']['id']], $user::TABLE);
 }
+$formation = $post->findOneBy(['id' => $curentuser->id_formation], $post::TABLE);
+
+
+
+
+// if ($_SESSION['autoriser'] != 'oui') {
+//     header('Location: ../templates/form_autho.php');
+//     exit();
+// }
+
 include 'inc_header.php';
 ?>
 
@@ -19,7 +31,7 @@ include 'inc_header.php';
             <ul class="list-style-none">
                 <li class="d-flex no-block card-body ">
                     <i class="fas fa-address-card mr-4 mt-3 icon-liste"></i>
-                    <div class=" mt-3"> <span class="">Des informations sur ma formations</span>
+                    <div class=" mt-3"> <span class="">Des informations sur ma formation</span>
                     </div>
                     <div class="ml-auto">
                         <button type="button" class="btn btn-primary btn-circle m-1" data-toggle="modal" data-target="#exampleModal">
@@ -49,42 +61,37 @@ include 'inc_header.php';
             </ul>
         </div>
 
-        <!-- modela number 1 -->
+        <!-- modal number 1 -->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title " id="exampleModalLabel">Ma formation</h5>
+                        <h5 class="modal-title " id="exampleModalLabel"><?= $formation->titel ?></h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="card-body mb-4">
+                    <div class="card-body mb-1">
                         <div class="card-body p-5">
-                            <h4 class="mb-4">List with bullets</h4>
-                            <!-- List with bullets -->
-                            <ul class="list-bullets">
-                                <a class="#">
-                                    <li class="mb-2">Lorem ipsum dolor sit amet.</li>
+                            <div class="d-flex flex-row mb-3">
+                                <?php
+                                echo  "<img src='../upload/" . $formation->picture . "'width='70' height='60'>";
+                                ?>
+                                <div class="d-flex flex-column ml-2">
+                                    <span><?= $formation->titel ?></span>
+                                    <span class="text-black-50">G7solution</span>
+                                </div>
+                            </div>
+                            <h6 class="mt-4"><?= $formation->description ?></h6>
+                            <div class="d-flex justify-content-between install mt-3"><span><?= $formation->createdAt ?></span>
+                                <a href="#" class="text-primary">Savoir plus&nbsp;
+                                    <i class="fa fa-angle-right"></i>
                                 </a>
-                                <a class="#">
-                                    <li class="mb-2">Consectetur adipisicing elit.</li>
-                                </a class="#">
-                                <a class="#">
-                                    <li class="mb-2">Sed do eiusmod tempor incididunt.</li>
-                                </a>
-                                <a class="#">
-                                    <li class="mb-2">Ut labore et dolore magna aliqua. </li>
-                                </a>
-                                <a class="#">
-                                    <li class="mb-2">Exercitation ullamco laboris nisi.</li>
-                                </a>
-                            </ul>
+                            </div>
                         </div>
                     </div>
                     <div class="modal-footer text-center">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-
                     </div>
                 </div>
             </div>
@@ -132,7 +139,6 @@ include 'inc_header.php';
 
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-
                     </div>
                 </div>
             </div>
