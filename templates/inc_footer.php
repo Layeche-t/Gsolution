d15e170a547a890d5e47dd7ac90b5a90c0d5ae3a
Layeche Torki <layeche.torki@gmail.com>
2021-09-09 23:47:58 +0200

    adding links to pages

--- templates/inc_footer.php
+++ templates/inc_footer.php
@@ -1,163 +1,68 @@
-<footer class="container-fluid bg-grey pt-5">
+<footer class="container-fluid menu ">
+	<div class="container">
+		<div class="row">
 
-	<div class="row">
-		<div class="col-md-6">
-			<div class="row">
-				<div class="col-md-6 ">
-					<div class="logo-part">
-						<img src="../pictures/qualiopi.png" class="w-50 logo-footer">
-						<p>7637 Laurel Dr. King Of Prussia, PA 19406</p>
-						<p>Use this tool as test data for an automated system or find your next pen</p>
-					</div>
-				</div>
+			<div class="col-md-6 pt-3">
+				<div class="row">
 
-				<div class="col-md-6 px-4">
-					<h6 class="font-weight-bold"> Nos offres</h6>
-					<div class="row ">
-						<div class="col-md-6 font-weight-bold">
-							<ul>
-								<li> <a href="#"> Formations à distence</a> </li>
-								<li> <a href="#"> Nos services</a> </li>
-								<li> <a href="#"> Sur mesure</a> </li>
-							</ul>
+					<div class="col-md-6 ">
+						<div class="logo-part">
+							<h1 class="font-weight-bold text-body">G7solution</h1>
+
+							<p class=" text-body py-2 pr-2 mb-0  "> Votre partenaire en conseil et formation, vous accompagne du diagnostic à la réalisation de vos projets de développement,
+								compétences et ressources : G7 Solution !
+							</p>
+							<div class="col  p-0 mb-2">
+								<img src="../pictures/qualiopi-removebg-preview.png" width="100px" height="60px">
+								<img src="../pictures/data-removebg-preview.png" width="100px" height="60px">
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<div class="col-md-6">
-					<div class="social">
-						<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-						<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<div class="col-md-6  px-5">
+						<h6 class="text-body font-weight-bold"> Nos offres </h6>
+						<a href="#" class="text-body font-weight-bold text-decoration-none"> Formations à distence </a><br>
+						<a href="#" class="text-body font-weight-bold text-decoration-none "> Nos services</a><br>
+						<a href="#" class="text-body font-weight-bold text-decoration-none"> Sur-mesure </a><br>
 					</div>
+
 				</div>
 			</div>
-		</div>
 
-		<div class="col-md-6">
-			<div class="row">
-				<div class="col-md-6 px-4">
-					<h6 class="font-weight-bold"> G7solution</h6>
-					<div class="row ">
-						<div class="col-md-6 font-weight-bold">
-							<ul>
-								<li> <a href="#"> Nous sommes</a> </li>
-								<li> <a href="#"> L'équipe</a> </li>
-								<li> <a href="#"> Nos experts</a> </li>
-								<li> <a href="#"> Blog</a> </li>
-							</ul>
+			<div class="col-md-6 pt-3">
+				<div class="row">
+					<div class="col-md-6 px-4">
+						<h6 class="text-body font-weight-bold "> G7solution</h6>
+						<div class="row ">
+							<div class="col-md-6">
+								<a class="text-body font-weight-bold text-decoration-none"> Nous sommes</a>
+								<a href="team" class="text-body font-weight-bold text-decoration-none"> L'équipe</a><br>
+								<a class="text-body font-weight-bold text-decoration-none"> Nos experts</a><br>
+								<a href="blog" class="text-body font-weight-bold text-decoration-none"> Blog</a>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<div class="col-md-6 ">
-					<h6 class="font-weight-bold"> En savoir plus </h6>
-					<div class="row ">
-						<div class="col-md-6 font-weight-bold">
-							<ul>
-								<li> <a href="#"> FQA</a> </li>
-								<li> <a href="#"> Contactez nous</a> </li>
-								<li> <a href="#"> Le plan du site</a> </li>
-							</ul>
-						</div>
+					<div class="col-md-6 ">
+						<h6 class="text-body font-weight-bold"> En savoir plus</h6>
+						<a href="askQuestions" class="text-body font-weight-bold text-decoration-none"> FQA</a> </li><br>
+						<a href="form_contact" class="text-body font-weight-bold text-decoration-none "> Contactez-nous</a> </li><br>
+						<a href="#" class="text-body font-weight-bold text-decoration-none"> Le plan du site</a> </li><br>
 					</div>
 				</div>
 			</div>
+
 		</div>
+
 	</div>
-
-	<div class="copyright">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-6">
-					<span>Copyright © 2019, All Right Reserved Seobin</span>
-				</div>
-				<!-- End Col -->
-				<div class="col-md-6">
-					<div class="copyright-menu">
-						<ul>
-							<li>
-								<a href="#">Home</a>
-							</li>
-							<li>
-								<a href="#">Terms</a>
-							</li>
-							<li>
-								<a href="#">Privacy Policy</a>
-							</li>
-							<li>
-								<a href="#">Contact</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<!-- End col -->
-			</div>
-			<!-- End Row -->
+	<div class="row div-logo text-center ">
+		<div class="social col pt-2">
+			<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+			<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
 		</div>
-		<!-- End Copyright Container -->
-	</div>
-	<!-- End Copyright -->
-	<!-- Back to top -->
-	<div id="back-to-top" class="back-to-top">
-		<button class="btn btn-dark" title="Back to Top" style="display: block;">
-			<i class="fa fa-angle-up"></i>
-		</button>
-	</div>
-
-</footer>
-
-
-
-</body>
-
-</html>
-
-
-
-
-
-<!-- Le premier bloc -->
-<!-- <div class="bloc1">
-		<div class="certifications">
-			<h2 class="titre-bloc1"><a class="titre-bloc1-a">Certifications</a></h2>
-
-			<div class="logos">
-				<img class="qualiopi1" src="../pictures/qualiopi.png">
-				<img class="qualiopi2" src="../pictures/data.jpg">
-			</div>
-
-			<h2 class="nos-offres">Nos offres</h2>
-			<h2 class="sous-text"><a href="#">Formations à distence</a></h2>
-			<h2 class="sous-text"><a href="#">Nos services</a></h2>
-			<h2 class="sous-text"><a href="#">Sur mesure</a></h2>
-		</div>
-	</div> -->
-
-<!-- Le deuxième bloc -->
-<!-- <div class="bloc2">
-		<h2 class="g7solution">G7solution</h2>
-		<h2 class="sous-text"><a href="#">Nous sommes</a></h2>
-		<h2 class="sous-text"><a href="../templates/team.php">L'équipe</a></h2>
-		<h2 class="sous-text"><a href="#">Nos experts</a></h2>
-		<h2 class="sous-text"><a href="blog.php">Blog</a></h2>
-	</div> -->
-
-
-<!-- Le troisème bloc -->
-<!-- <div class="bloc3">
-		<h2 class="titre-bloc3"><a class="titre-bloc1-a">Rejoignez-nous</a></h2>
-
-		<div class="logos">
-			<i class="fab fa-facebook-square ico"></i>
-			<i class="fab fa-twitter-square ico"></i>
-			<i class="fab fa-linkedin ico"></i>
-			<i class="fab fa-youtube ico"></i>
+		<div class="">
+			<p class="font-weight-bold mb-2">© G7solution - Tous droits réservés</p>
 		</div>
 
-		<h2 class="nos-offres">En savoir plus</h2>
-		<h2 class="sous-text"><a href="faq.php">FAQ</a></h2>
-		<h2 class="sous-text"><a href="formulaire_contact.php">Contactez-nous</a></h2>
-		<h2 class="sous-text"><a href="#">Le plan du site</a></h2>
 	</div>
 </footer>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
