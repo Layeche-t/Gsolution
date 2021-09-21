4e54dd163ce1afedacdee0afb53a096dd3403bd9
Layeche Torki <layeche.torki@gmail.com>
2021-09-21 22:11:03 +0200

    add comments

--- controllers/add_users.php
+++ controllers/add_users.php
@@ -5,9 +5,16 @@ $user =  new User();
 
 if (isset($_POST['validation'])) {
 
+
+
     unset($_POST['validation']);
 
+
+
     $newPost = $user->SetUser($_POST);
+
+
+
 
     header('Location: ../admin/users_disply.php?success');
     exit;
