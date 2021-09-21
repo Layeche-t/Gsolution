183103df45c925a34c3ef6f4d347de107ec3a386
Layeche Torki <layeche.torki@gmail.com>
2021-09-21 22:03:35 +0200

    add background color

--- templates/opinion.php
+++ templates/opinion.php
@@ -1,14 +1,15 @@
-<div class="container mt-5 border">
-    <h3 class="mb-4 font-weight-bold titel"> Ce qu'ils disent de nous </h3>
-    <div class="col-lg-10 offset-lg-1 bg-dark text-light">
+<div class="container my-5 ">
+    <h3 class="mb-4 font-weight-bold titel "> Ce qu'ils disent de nous </h3>
+    <div class="carousel-opinion col-lg-10 offset-lg-1 text-light  ">
         <div id="client-testimonial-carousel" class="carousel slide" data-ride="carousel" style="height:200px;">
+
             <div class="carousel-inner" role="listbox">
 
-                <div class="carousel-item active text-center p-5">
+                <div class="carousel-item active text-center p-4">
                     <blockquote class="blockquote text-center">
-                        <p class="mb-0"><i class="fa fa-quote-left"></i> Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.
+                        <p class="mb-0 font-weight-bold"><i class="fa fa-quote-left"></i> Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.
                         </p>
-                        <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+                        <footer class="blockquote-footer font-weight-bold">Albert Einstein <br><cite title="Source Title">genius</cite></footer>
                         <!-- Client review stars -->
                         <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
                         <p class="client-review-stars">
@@ -20,11 +21,12 @@
                         </p>
                     </blockquote>
                 </div>
+
                 <div class="carousel-item text-center p-4">
                     <blockquote class="blockquote text-center">
-                        <p class="mb-0"><i class="fa fa-quote-left"></i> Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.
+                        <p class="mb-0 font-weight-bold"><i class="fa fa-quote-left"></i> Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.
                         </p>
-                        <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+                        <footer class="blockquote-footer font-weight-bold">Albert Einstein <br><cite title="Source Title">genius</cite></footer>
                         <!-- Client review stars -->
                         <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
                         <p class="client-review-stars">
@@ -36,11 +38,12 @@
                         </p>
                     </blockquote>
                 </div>
+
                 <div class="carousel-item text-center p-4">
                     <blockquote class="blockquote text-center">
-                        <p class="mb-0"><i class="fa fa-quote-left"></i> A person who never made a mistake never tried anything new.
+                        <p class="mb-0 font-weight-bold"><i class="fa fa-quote-left"></i> Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.
                         </p>
-                        <footer class="blockquote-footer">Albert Einstein <cite title="Source Title">genius</cite></footer>
+                        <footer class="blockquote-footer font-weight-bold ">Albert Einstein <br><cite title="Source Title">genius</cite></footer>
                         <!-- Client review stars -->
                         <!-- "fas fa-star" for a full star, "far fa-star" for an empty star, "far fa-star-half-alt" for a half star. -->
                         <p class="client-review-stars">
@@ -52,6 +55,7 @@
                         </p>
                     </blockquote>
                 </div>
+
             </div>
             <ol class="carousel-indicators">
                 <li data-target="#client-testimonial-carousel" data-slide-to="0" class="active"></li>
