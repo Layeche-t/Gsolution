bb4b64160ac3b838bf0e1db69f19e17e5a01a4c6
Layeche Torki <layeche.torki@gmail.com>
2021-09-21 22:09:15 +0200

    added security through checks

--- controllers/add.php
+++ controllers/add.php
@@ -2,18 +2,36 @@
 require_once('../inc_config.php');
 
 $user = new User();
+$post = new Post();
 $now = new DateTime();
 
-if (isset($_POST['validation'])) {
+if (
+    isset($_POST['validation'])
+    && isset($_POST['firstname']) && isset($_POST['role'])
+    && isset($_POST['lastname']) && isset($_POST['code'])
+    && isset($_POST['email']) && isset($_POST['password']) &&
+    isset($_POST['confirmPassword'])
+    && !empty($_POST['firstname']) && !empty($_POST['role'])
+    && !empty($_POST['lastname']) && !empty($_POST['code'])
+    && !empty($_POST['email']) && !empty($_POST['password']) &&
+    !empty($_POST['confirmPassword'])
+) {
+
 
     unset($_POST['validation']);
 
+    $checkPosr = $post->findOneBy(['code' => $_POST['code']], $post::TABLE);
+
+    if ($checkPosr->code !== $_POST['code']) {
+        header('Location: ../templates/registrationForm.php?error=cod');
+        exit();
+    }
 
     if ($_POST['password'] != $_POST['confirmPassword']) {
         header('Location: ../templates/registrationForm.php?error=pw');
         exit();
     }
-
+    unset($_POST['code']);
     unset($_POST['confirmPassword']);
     $check = $user->findOneBy(['email' => $_POST['email']], $user::TABLE);
 
@@ -27,4 +45,7 @@ if (isset($_POST['validation'])) {
 
     header('Location: ../templates/form_autho.php?success');
     exit;
+} {
+    header('Location: ../templates/registrationForm.php?error=vd');
+    exit();
 }
