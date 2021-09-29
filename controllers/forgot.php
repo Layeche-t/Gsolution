b54c2fc2f5d4d33f049640238a3218745cbc5edf
Layeche Torki <layeche.torki@gmail.com>
2021-09-29 14:10:48 +0200

    change of sending message

--- controllers/forgot.php
+++ controllers/forgot.php
@@ -15,30 +15,33 @@ if (isset($_POST['email']) && !empty($_POST['email'])) {
     // email construction
     if ($check) {
         $string = implode('', array_merge(range('A', 'Z'), range('a', 'z'), range('0', '9')));
+
         $mini = strtolower($string);
+
         // token creation
         $token = substr(str_shuffle($mini), 0, 20);
         $_POST['newCreat'] = $now->format('Y-m-d H:i:s');
         $update = $user->updateById(['token' => $token, 'newCreat' => $_POST['newCreat'], 'id' => $check->id], $user::TABLE);
         $link = "http://localhost/Gsolution/controllers/recover?token=$token";
+
+        //the elements of the message
         $to = $_POST['email'];
-        $subject = 'Réinitialisation de votre mot de passe';
-        $headers = [];
-        $headers[] = "MIME-Version: 1.0\r\n";
-        $headers[] = "Content-type: text/html; charset=iso-8859-1\r\n";
-        $message = "Bonjour, voici votre nouveau mot de passe : <a href=" . $link . "> $link </a>";
+        $subject = 'Réinitialisation du mot de passe';
+        $message = 'Bonjour, ci-joint le lien qui vous permettre de réinitialisation votre mot de passe: ' . $link;
 
         // sending email
-        mail($to, $subject, $message, implode($headers));
+        mail($to, $subject, $message);
         // redirect
         header('Location: ../templates/forgot_password.php?success');
         exit;
     } else {
-        // redirect
+        // redirect if email not exist 
+
         header('Location: ../templates/forgot_password.php?error=noc');
         exit;
     }
 } else {
+    // redirect if email empty 
     header('Location: ../templates/forgot_password.php?error=vid');
     exit;
 }
