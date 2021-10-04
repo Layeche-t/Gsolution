b873cfb063239a69e052450f215c554e8a9c19bc
Layeche Torki <layeche.torki@gmail.com>
2021-10-04 21:32:01 +0200

    format the page

--- templates/page.php
+++ templates/page.php
@@ -14,19 +14,20 @@ include 'inc_header.php'; ?>
 <div class="container-fluid ">
 
     <div class="container mb-4 ">
-        <h3 class="mt-4 ml-4 mb-0 font-weight-bold titel  ">
+        <h3 class="mt-4 ml-4 mb-0 font-weight-bold titel">
             <?= $slider->titel ?>
         </h3>
 
         <div class="row">
             <div class="btn-group btn-breadcrumb ml-4 mb-2">
-                <a href="home_display" class="btn btn-default "><i class="fas fa-home"></i></a>
+                <a href="index.php" class="btn btn-default "><i class="fas fa-home"></i></a>
+                <a href="#" class="btn btn-default font-weight-bold a-color"><?= $slider->type ?> <i class="fas fa-chevron-right"></i></a>
                 <a href="#" class="btn btn-default font-weight-bold a-color"><?= $slider->titel ?> <i class="fas fa-chevron-right"></i></a>
             </div>
         </div>
 
-        <div class="row text-secondary ml-4 mb-2 ">
-            <?= $slider->description ?>
+        <div class="row text-secondary ml-4 mb-2">
+            <p class="width-element"><?= $slider->description ?></p>
         </div>
         <div class="row ml-4 text-center ">
             <?php
@@ -34,11 +35,11 @@ include 'inc_header.php'; ?>
             ?>
         </div>
         <div class="row  ml-4">
-            <h6 class="text-secondary "> La source : <?= $slider->source ?></h6>
+            <h6 class="text-secondary"> Source : <?= $slider->source ?></h6>
         </div>
 
-        <div class="row mt-3 ml-4 w-20 " style="width: 80%;">
-            <p><?= $slider->text ?></p>
+        <div class="row mt-3 ml-4">
+            <p class="width-element"><?= $slider->text ?></p>
         </div>
     </div>
 
