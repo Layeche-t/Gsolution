2048bea92a9e9f2e65a9c7e08b851e7fde33e60f
Layeche Torki <layeche.torki@gmail.com>
2021-10-04 21:31:05 +0200

    adding an errors

--- controllers/add_blog.php
+++ controllers/add_blog.php
@@ -13,11 +13,15 @@ if (isset($_POST['validation'])) {
 
     unset($_POST['validation']);
     $fileName = $fileManager->UploadImage($_FILES['picture']);
+
+    if ($fileName == 'size') {
+        header('Location: ../admin/' . $_SESSION['info']['redirect'] . '.php?size');
+        exit;
+    }
     $_POST['picture'] = $fileName;
     $newPost = $post->set($_POST, $_SESSION['info']['table']);
 
 
     header('Location: ../admin/' . $_SESSION['info']['redirect'] . '.php?success');
-
     exit;
 }
